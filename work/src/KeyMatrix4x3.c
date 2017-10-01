c1b3c74503978f5ff82a468a1853ca28e8a5f288
gumboss <gumboss@web.de>
2017-10-01 14:42:53 +0200

    add Matrix4x3 src datas with testprogramm

---
+++ work/src/KeyMatrix4x3.c
@@ -0,0 +1,102 @@
+#include "KeyMatrix.h"
+
+int init(KeyMatrix* KM){
+	int i,r,c = 0;
+	
+	KM->rows = ROWS;
+	KM->columns = COLUMNS;
+
+	for(r=0;r<KM->rows;r++){
+		for(c=0;c<KM->columns;c++){
+			KM->keyState[r][c]=0;
+		}
+	}
+	
+	keyPressed = NULL;
+	keyPressedMapped = NULL;
+
+	KM->keyMaping[0][0] = 1;
+	KM->keyMaping[0][1] = 2;
+	KM->keyMaping[0][2] = 3;
+
+	KM->keyMaping[1][0] = 4;
+	KM->keyMaping[1][1] = 5;
+	KM->keyMaping[1][2] = 6;
+
+	KM->keyMaping[2][0] = 7;
+	KM->keyMaping[2][1] = 8;
+	KM->keyMaping[2][2] = 9;
+
+	KM->keyMaping[3][0] = 0;
+	KM->keyMaping[3][1] = 0;
+	KM->keyMaping[3][2] = 0;
+
+	KM->rowPin[0] = 3;
+	KM->rowPin[1] = 4;
+	KM->rowPin[2] = 5;
+	KM->rowPin[3] = 6;
+	
+	KM->columnPin[0] = 0;
+	KM->columnPin[1] = 1;
+	KM->columnPin[2] = 2;
+	
+	for(r=0;r<KM->rows;r++){
+		pinMode(KM->rowPin[r], OUTPUT);
+	}
+
+	for(c=0;c<KM->columns;c++){
+		pinMode(KM->columnPin[c], INPUT);		
+	}
+}
+
+int scanKeys(KeyMatrix* KM){
+	
+	KM->keyPressed=0;										//Zurücksetzten vor dem erneuten scannen
+	for(r=0;r<KM->rows;r++){
+		
+		digitalWrite(KM->rowPin[r],1);
+
+		for(c=0;c<KM->columns;c++){
+										
+			if(digitalRead(KM->columnPin[c]) == 1){
+				if(	KM->keyState[r][c] == 0){				//Wird nur beim Pressen einer Taste gesetzt
+					KM->keyPressed=1;
+					KM->lastPressed=KM->keyMapping[r][c];}
+				KM.keyState[r][c]=1;
+			}else
+				KM.keyState[r][c]=0;
+		}
+		 digitalWrite(KM.rowPin[r],0);
+	}
+	
+}
+
+int waitForRelease(KeyMatrix* KM){
+	
+	while(1){
+		scanKeys(KM);
+		if(KM->keyPressed == 1)
+			return 0;
+		usleep(100);
+	}
+	
+}
+
+int main(int argc, char *argv[]){
+	
+	KeyMatrix KeyMatrix4x3;
+	int i= 0;
+	printf("Start MatrixPad Test\n");
+	
+	if(wiringPiSetup() != 0)
+		printf("ERROR PiSetup()\n");
+	else 
+		printf("PiSetup succsess\n");
+
+	init(&KeyMatrix4x3);
+	
+	for(i=0;i<5;i++){
+		waitForRelease(&KeyMatrix4x3);
+		printf("KesPressed: %i\n",KeyMatrix4x3.lastPressed);
+	}
+}
---
+++ work/src/KeyMatrix4x3.h
@@ -0,0 +1,24 @@
+include <stdio.h>
+#include <stdlib.h>
+
+#include <wiringPi.h>
+
+#define ROWS 4
+#define COLUMNS 3
+
+typedef struct {
+	int rows;
+	int columns;
+	int keyState[ROWS][COLUMNS];
+	int keyMapping[ROWS][COLUMNS];
+	int rowPin[ROWS], columnPin[COLUMNS];
+	int keyPressed;		// Zeigt an, ob seid dem letzten aufruf von scanKeys() eine Taste gedrückt wurde.
+	int lastPressed;	// Zeigt an, welche Taste waehrend dem letzten aufruf von scanKeys() gedrueckt war.
+
+} KeyMatrix;
+
+int init(KeyMatrix* KM);
+
+int scanKeys(KeyMatrix* KM);
+
+int waitForRelease(KeyMatrix* KM);
